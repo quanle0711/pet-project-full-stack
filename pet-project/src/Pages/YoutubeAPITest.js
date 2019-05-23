b2ea8ccafcd1349b53faf706a6275c89dd4f3169
lequan1 <lequan1@ecs.vuw.ac.nz>
2019-05-23 20:54:03 +1200

    working on Youtube

--- pet-project/src/Pages/YoutubeAPITest.js
+++ pet-project/src/Pages/YoutubeAPITest.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { generateKeyPair } from 'crypto';
 
 const API_KEY = "AIzaSyDFyK14g41ldTRl8sKTWf4l4i2dkiDf-_k";
-const CLIENT_ID = "302303575258-uv6ba1lmhtubbqarrtojgmn06iu4iqcr.apps.googleusercontent.com"; 
+const CLIENT_ID = "302303575258-uv6ba1lmhtubbqarrtojgmn06iu4iqcr.apps.googleusercontent.com";
+var resultNum = 10;
+const finalURL = `https://www.googleapis.com/youtube/v3/search?part=snippet,id&order=date&key=${API_KEY}&maxResults=${resultNum}`;
 
-function auth() {
+//?&maxResults=25&q=surfing&key=
+
+class YoutubeAPI extends Component {
+
+constructor(props) {
+    super(props);
+
+    this.state = {
+        //  TODO
+    }
 }
 
+    render() {
 
-
-const YoutubeAPI = () => {
-    return (
-        <div><p>Youtube API page</p></div>
-    );
+        return (
+            <div><p>Youtube API page</p></div>
+        );
+    }
 };
 
 export default YoutubeAPI;
