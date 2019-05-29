528b80367c4ed19616bbb1dc484b5930af48f1a5
lequan1 <lequan1@ecs.vuw.ac.nz>
2019-05-30 10:26:03 +1200

    searchbar api now works (spent) 40~ mins

--- pet-project/src/Pages/YoutubeAPITest.js
+++ pet-project/src/Pages/YoutubeAPITest.js
@@ -1,40 +1,40 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
 
 const API_KEY = "AIzaSyDFyK14g41ldTRl8sKTWf4l4i2dkiDf-_k";
 const CLIENT_ID =
     "302303575258-uv6ba1lmhtubbqarrtojgmn06iu4iqcr.apps.googleusercontent.com";
-var resultNum = 8;
-var query = "johnwick3";
-const finalURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=${query}&key=${API_KEY}&maxResults=${resultNum}`;
+var resultNum = 10;
+var query = "";
 
-//?&maxResults=25&q=surfing&key=
+
 
 class YoutubeAPI extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            //  TODO
             videoResults: [],
+            queryyt:"",
             isLoading: false
         };
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
+
+        var finalURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&type=video&q=${this.state.queryyt}&key=${API_KEY}&maxResults=${resultNum}`;
+
         this.setState({ isLoading: true }, () => {
             fetch(finalURL)
-                .then(res => res.json())
+                .then(res => res.json()) //convert to json
                 .then(resJson => {
                     const vids = resJson.items
-                        .map(object => "https://youtube.com/embed/"+object.id.videoId)
-                        .filter(e => {
-                            return e;
-                        });
-
-                    this.setState({ videoResults: vids });
-                    //console.log(this.state.videoResults);
+                        .map(object => "https://youtube.com/embed/" + object.id.videoId);
+                    this.setState({ videoResults: vids });//maps video links to state
+                    console.log(this.state.videoResults);
                 })
                 .catch(err => {
                     console.error(err);
@@ -42,31 +42,42 @@ class YoutubeAPI extends Component {
         });
     }
 
+    handleInputChange = event => {
+        this.setState({queryyt : event.target.value})
+    }
+
     render() {
-        console.log(finalURL);
+        //console.log(finalURL);
 
         return (
             <div>
                 <h1>Youtube API page</h1>
                 <hr />
-                <Button
-                    variant="success"
-                    size="lg"
-                    block
-                    onClick={this.handleClick}
-                >
-                    Load vids
-                </Button>
+                <InputGroup>
+                    <Form.Control
+                        value={this.state.query}
+                        placeholder="search for keywords here..."
+                        onChange={this.handleInputChange}
+                    />
+
+                    <InputGroup.Append>
+                        <Button
+                            variant="outline-success"
+                            onClick={this.handleClick}
+                        >Search</Button>
+                    </InputGroup.Append>
+                </InputGroup>
 
                 <div>
                     {
                         this.state.videoResults.map((links, i) => {
-                            var frame = <div key = {i}><iframe width = "640" height = "360" src = {links} frameBorder="0" allowFullScreen></iframe></div>
+                            var frame = <div key={i}><iframe width="640" height="360" src={links} frameBorder="0" allowFullScreen></iframe></div>
                             return frame;
                         })
                     }
                     {this.frame}
                 </div>
+                <hr/>
             </div>
         );
     }
