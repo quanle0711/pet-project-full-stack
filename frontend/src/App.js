f1187d64298d44705b0d6bec579dfc7d008ea813
Quan Le <lequan1@myvuw.ac.nz>
2019-07-04 13:24:19 +1200

    created a frame for front & back end

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+//router imports
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    NavLink
+} from "react-router-dom";
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    <h1>hello world this is Front End.</h1>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
